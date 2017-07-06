80c571cce0548a8a2795737fbd8395894ecc2a27
Shengbo.Xu <shengbo.xu@medley.jp>
2017-07-06 22:49:14 +0900

    add: contents 2017年 7月 6日 木曜日 22時49分14秒 JST

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,8 +1,8 @@
+//= require vendor/modernizr-2.6.2.min
+//= require vendor/jquery.min
 //= require vendor/bootstrap.min
 //= require vendor/jquery.fancybox
-//= require vendor/jquery.min
 //= require vendor/main
-//= require vendor/modernizr-2.6.2.min
 //= require vendor/owl.carousel.min
 //= require vendor/slider
 //= require vendor/wow.min
